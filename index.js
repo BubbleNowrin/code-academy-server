dd6ea3718963c0a7d4a907b36d669f982260f630
NowrinIslamMim <nowrin02@gmail.com>
2022-10-25 00:40:47 +0600

    checking server working properly

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Code Academy Running')
+})
+
+app.listen(port, () => {
+    console.log(`Code Acdemy listening on port ${port}`)
+})
