244d8a708edbf266869c5cb8cd7ccb76f25258d5
NowrinIslamMim <nowrin02@gmail.com>
2022-10-25 04:23:58 +0600

    dynamic api route created

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json')
+
 const cors = require('cors');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Code Academy Running')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(cr => cr.id == id)
+    res.send(course)
 })
 
 app.listen(port, () => {
